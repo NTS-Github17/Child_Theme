6630c28af2f53d1c2fa4b3131a63f1fe8e7d4bf7
NTS-Github17 <phanvantruong1705@gmail.com>
2023-12-27 15:06:02 +0700

    Added child theme

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$THEME->doctype = 'html5';
+$THEME->name = 'vars'; // Name of the theme
+$THEME->sheets = array(''); // Stylesheets for the theme
+$THEME->editor_sheets = [];
+$THEME->parents = ['boost']; // Parent theme
+$THEME->enable_dock = false;
+$THEME->yuicssmodules = array();
+$THEME->javascripts = array(''); // JS Files for the theme
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->csspostprocess = 'theme_remui_process_css';
+$THEME->requiredblocks = '';
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+$THEME->hidefromselector = false;
+$THEME->haseditswitch = true;
